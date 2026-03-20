d0a40b25860da7c9e21986027945f89c5a6cf35f
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2026-03-20 12:54:23 +0100

    chore: upgrade DigitalOcean provider to >= 2.70.0 and Terraform to >= 1.10.0 (#48)
    * ⬆️ upgrade: update DigitalOcean provider to >= 2.70.0 and Terraform to >= 1.10.0
    
    * fix: correct required_version to >= 1.10.0
    
    ---------
    
    Co-authored-by: Anmol Nagpal <anmolnagpal@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.5.4"
+  required_version = ">= 1.10.0"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = ">= 2.79.0"
+      version = ">= 2.70.0"
     }
   }
 }
