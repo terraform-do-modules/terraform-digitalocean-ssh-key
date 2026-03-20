b76b1e7dd792de4ad1959c25319948d5e5d05dd0
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-20 07:22:32 -0400

    ⬆️ upgrade: update DigitalOcean provider to >= 2.70.0 and Terraform to >= 1.10.0

--- _examples/basic/versions.tf
+++ _examples/basic/versions.tf
@@ -1,11 +1,11 @@
 # Terraform version
 # Compatible with OpenTofu >= 1.6.0 (https://opentofu.org)
 terraform {
-  required_version = ">= 1.5.4"
+  required_version = ">= 2.70.0"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = ">= 2.29.0"
+      version = ">= 2.70.0"
     }
   }
 }
