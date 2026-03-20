6a13549036fd9074699d41aa9632c4fe643d1471
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-20 07:38:13 -0400

    fix: correct required_version to >= 1.10.0

--- _examples/basic/versions.tf
+++ _examples/basic/versions.tf
@@ -1,7 +1,7 @@
 # Terraform version
 # Compatible with OpenTofu >= 1.6.0 (https://opentofu.org)
 terraform {
-  required_version = ">= 2.70.0"
+  required_version = ">= 1.10.0"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
